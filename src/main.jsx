bd6d7911acac0ad530ab8c14c16b21e933de6338
ojairoluis <ojairoluis@gmail.com>
2025-10-04 22:42:33 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'  // 👈 esta línea es clave
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
